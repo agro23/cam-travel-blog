a99d75ac14a8f0002687f2dc96d7016431c41794
Sara Hamilton and Cameron Anderson <sara@indeterminate.net and chamburg321@gmail.com>
2018-04-18 11:17:57 -0700

    add index view and controller for locations

---
+++ TravelBlog/Controllers/LocationsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TravelBlog.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TravelBlog.Controllers
+{
+    public class LocationsController : Controller
+    {
+        private TravelDbContext db = new TravelDbContext();
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
