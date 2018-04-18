9b9d42cc838c4b9323697040063d5f0dfadb6e8f
Sara Hamilton and Cameron Anderson <sara@indeterminate.net and chamburg321@gmail.com>
2018-04-18 14:01:23 -0700

    add CRUD for People

---
+++ TravelBlog/Controllers/PeopleController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TravelBlog.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TravelBlog.Controllers
+{
+    public class PeopleController : Controller
+    {
+        private TravelDbContext db = new TravelDbContext();
+        public IActionResult Index()
+        {
+            List<Person> model = db.People.ToList();
+            return View(model);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Person person)
+        {
+            db.People.Add(person);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var thisPerson = db.People.FirstOrDefault(People => People.PersonId == id);
+            return View(thisPerson);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Person person)
+        {
+            db.Entry(person).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var thisPerson = db.People.FirstOrDefault(People => People.PersonId == id);
+            return View(thisPerson);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id) 
+        {
+            var thisPerson = db.People.FirstOrDefault(People => People.PersonId == id);
+            db.People.Remove(thisPerson);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
