15d7b1fa68e4cba18f910f91c6a85d221d5d0da3
Sara Hamilton and Cameron Anderson <sara@indeterminate.net and chamburg321@gmail.com>
2018-04-18 14:22:18 -0700

    add CRUD for experiences

---
+++ TravelBlog/Controllers/ExperiencesController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TravelBlog.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TravelBlog.Controllers
+{
+    public class ExperiencesController : Controller
+    {
+        private TravelDbContext db = new TravelDbContext();
+        public IActionResult Index()
+        {
+            List<Experience> model = db.Experiences.ToList();
+            return View(model);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Experience experience)
+        {
+            db.Experiences.Add(experience);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var thisExperience = db.Experiences.FirstOrDefault(Experiences => Experiences.ExperienceId == id);
+            return View(thisExperience);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Experience experience)
+        {
+            db.Entry(experience).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Details(int id)
+        {
+            var thisExperience = db.Experiences.FirstOrDefault(Experiences => Experiences.ExperienceId == id);
+            return View(thisExperience);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var thisExperience = db.Experiences.FirstOrDefault(Experiences => Experiences.ExperienceId == id);
+            return View(thisExperience);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var thisExperience = db.Experiences.FirstOrDefault(Experiences => Experiences.ExperienceId == id);
+            db.Experiences.Remove(thisExperience);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
